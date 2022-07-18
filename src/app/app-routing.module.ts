940cdffb4ab4c63126ce6024c1f9c35380189086
theophilewall <66305625+theophile-wallez@users.noreply.github.com>
2022-07-18 19:25:33 +0200

    add: handling of incorrect paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: 'listing', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
