c8251ce751b0810243ce1c7375cc9a51d07f5272
theophilewall <66305625+theophile-wallez@users.noreply.github.com>
2022-07-24 02:50:10 +0200

    changed about text

--- src/app/skills-browser-mock/skills-browser-mock.component.ts
+++ src/app/skills-browser-mock/skills-browser-mock.component.ts
@@ -17,9 +17,9 @@ export class SkillsBrowserMockComponent implements OnInit {
   ngOnInit(): void {
     this.techSides.set('frontend', [
       { name: 'Angular', logoName: 'angular.svg' },
-      { name: 'PrimeNG', logoName: 'primeng.svg' },
       { name: 'TypeScript', logoName: 'typescript.svg' },
       { name: 'JavaScript', logoName: 'javascript.svg' },
+      { name: 'PrimeNG', logoName: 'primeng.svg' },
       { name: 'HTML', logoName: 'html.svg' },
       { name: 'Sass', logoName: 'sass.svg' },
     ]);
@@ -33,12 +33,12 @@ export class SkillsBrowserMockComponent implements OnInit {
     ]);
 
     this.techSides.set('tools', [
-      { name: 'VS Code', logoName: 'vs-code.svg' },
-      { name: 'IntelliJ', logoName: 'intellij-idea.svg' },
-      { name: 'VS', logoName: 'visual-studio.svg' },
       { name: 'Git', logoName: 'git.svg' },
       { name: 'Github', logoName: 'github.svg' },
       { name: 'SVN', logoName: 'svn.svg' },
+      { name: 'VS Code', logoName: 'vs-code.svg' },
+      { name: 'IntelliJ', logoName: 'intellij-idea.svg' },
+      { name: 'VS', logoName: 'visual-studio.svg' },
     ]);
   }
 
